e3ebc38d71be3df943f6b872656ef67a522766cd
kw <waeschle@ub.uni-heidelberg.de>
2015-09-21 10:31:15 +0200

    Doc fix, got rid of unused code

--- OAIMetadataFormat_XMDP.inc.php
+++ OAIMetadataFormat_XMDP.inc.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * @defgroup oai_format_dc Dublin Core OAI format plugin
+ * @defgroup oai_format_xmdp XMetaDissPlus OAI format plugin
  */
 
 /**
- * @file plugins/oaiMetadataFormats/dc/OAIMetadataFormat_DC.inc.php
+ * @file plugins/oaiMetadataFormats/xmdp/OAIMetadataFormat_XMDP.inc.php
  *
  * Copyright (c) 2014-2015 Simon Fraser University Library
  * Copyright (c) 2003-2015 John Willinsky
@@ -30,39 +30,6 @@ class OAIMetadataFormat_XMDP extends OAIMetadataFormat {
 		$publicationFormat =& $record->getData('publicationFormat');
 		$description = $publicationFormat->extractMetadata(new Xmdp22Schema());
 		
-//		error_log(var_export($description, true));
-		
-/* 		$response = "<xMetaDiss:xMetaDiss\n" . 
-			"\txmlns:xMetaDiss=\"http://www.d-nb.de/standards/xmetadissplus/\"\n" .
-			"\txmlns:cc=\"http://www.d-nb.de/standards/cc/\"\n" .
-			"\txmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" .
-			"\txmlns:dcmitype=\"http://purl.org/dc/dcmitype\"\n" .
-			"\txmlns:dcterms=\"http://purl.org/dc/terms/\"\n" .
-			"\txmlns:ddb=\"http://www.d-nb.de/standards/ddb/\"\n" .
-			"\txmlns:dini=\"http://www.d-nb.de/standards/xmetadissplus/type/\"\n" .
-			"\txmlns:doi=\"http://www.d-nb.de/standards/doi/\"\n" .
-			"\txmlns:hdl=\"http://www.d-nb.de/standards/hdl/\"\n" .
-			"\txmlns:pc=\"http://www.d-nb.de/standards/pc/\"\n" .
-			"\txmlns=\"http://www.d-nb.de/standards/subject/\"\n" .
-			"\txmlns:thesis=\"http://www.ndltd.org/standards/metadata/etdms/1.0/\"\n" .
-			"\txmlns:urn=\"http://www.d-nb.de/standards/urn/\"\n" .
-			"\txsi:schemaLocation=\"http://www.d-nb.de/standards/xmetadissplus/ http://www.d-nb.de/standards/xmetadissplus/xmetadissplus.xsd\"\n" .
-			"\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n";
-		
-		foreach($description->getProperties() as $propertyName => $property) {
-			if ($description->hasStatement($propertyName)) {
-				if ($property->getTranslated()) {
-					$values = $description->getStatementTranslations($propertyName);
-				} else {
-					$values = $description->getStatement($propertyName);
-				}
-				$response .= $this->formatElement($propertyName, $values, $property->getTranslated());
-			}
-		}
-
-		$response .= "</xMetaDiss:xMetaDiss>"; */
-		
-		
 		//FIXME: Hack to remove the duplicate XML document declarations
 		$xmlFilter = new Xmdp22DescriptionXmlFilter(PersistableFilter::tempGroup(
 				'metadata::plugins.metadata.xmdp22.schema.Xmdp22Schema(*)',
@@ -70,42 +37,6 @@ class OAIMetadataFormat_XMDP extends OAIMetadataFormat {
 		
 		$response = substr($xmlFilter->process($description), 39);
 		
-		return $response;
-	}
-
-	/**
-	 * Format XML for single DC element.
-	 * @param $propertyName string
-	 * @param $value array
-	 * @param $multilingual boolean optional
-	 */
-	function formatElement($propertyName, $values, $multilingual = false) {
-		if (!is_array($values)) $values = array($values);
-
-		// Translate the property name to XML syntax.
-		$openingElement = str_replace(array('[@', ']'), array(' ',''), $propertyName);
-		$closingElement = String::regexp_replace('/\[@.*/', '', $propertyName);
-
-		//error_log("o: " . var_export($propertyName, true) . var_export($values, true));
-
-		// Create the actual XML entry.
-		$response = '';
-		foreach ($values as $key => $value) {
-			if ($multilingual) {
-				$key = str_replace('_', '-', $key);
-				assert(is_array($value));
-				foreach ($value as $subValue) {
-					if ($key == METADATA_DESCRIPTION_UNKNOWN_LOCALE) {
-						$response .= "\t<$openingElement>" . OAIUtils::prepOutput($subValue) . "</$closingElement>\n";
-					} else {
-						$response .= "\t<$openingElement xml:lang=\"$key\">" . OAIUtils::prepOutput($subValue) . "</$closingElement>\n";
-					}
-				}
-			} else {
-				assert(is_scalar($value));
-				$response .= "\t<$openingElement>" . OAIUtils::prepOutput($value) . "</$closingElement>\n";
-			}
-		}
 		return $response;
 	}
 }
